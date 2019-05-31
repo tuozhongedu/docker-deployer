27fc3a9d1fae3eaa9d8b8b46190a6c4e898b0b92
Cong Peijun <me@congpeijun.com>
2019-05-31 11:18:07 +0800

    Update Dockerfile.php

--- Dockerfile.php
+++ Dockerfile.php
@@ -3,4 +3,5 @@ FROM composer:latest
 RUN set -x && \
         apk --no-cache add rsync && \
         composer global require deployer/deployer && \
-        composer global require deployer/recipes
+        composer global require deployer/recipes && \
+        ln -s /tmp/vendor/bin/dep /usr/local/bin/dep
